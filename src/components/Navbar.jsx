71b5ab148c4ee856ab0383c7e58f017bb968ee7d
akramgadir <akram.gadir@gmail.com>
2023-12-04 18:16:23 +0000

    mobile navbar pop up fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ return (
   </ul>
 
 <div className='sm:hidden flex flex-1 justify-end items-center'>
-<img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer'
+<img src={toggle ? menu : close} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer'
  onClick={()=>setToggle(!toggle)}/>
     <div className={`${toggle? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
       <ul className='list-none flex justify-end items-start flex-col gap-4'>
