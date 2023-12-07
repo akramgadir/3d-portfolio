05834ca971dc16eb50d2b936fceb3f4290a1cbdf
akramgadir <akram.gadir@gmail.com>
2023-12-07 16:37:32 +0000

    emailjs service fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,8 +35,10 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_4vaw7eb',
+        'template_iq5ek3w',
+        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Akram",
@@ -44,7 +46,8 @@ const Contact = () => {
           to_email: "akram.gadir@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '2YGc125aRiGthRM8S'
+        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
